5e6076a819c98db0bb990a7054d6e8bfd8b82a4d
vitmat1 <103237429+vitmat1@users.noreply.github.com>
2022-07-05 20:58:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnRed = document.getElementById('btnRed');
+console.dir(btnRed);
+
+btnRed.onclick = function(){
+    alert('на мена нажали правой кнопкой')
+};
+btnRed.onmousemove = function(){
+    alert('на меня нажали много раз')
+};
